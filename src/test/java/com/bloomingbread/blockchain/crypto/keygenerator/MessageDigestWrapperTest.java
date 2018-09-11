529cea156360060c2586756e4f6c667259479921
Sangchual Cha <sangchual.cha@hotmail.com>
2018-09-10 23:46:56 -0400

    fix typo in MessageDigestWrapper class

--- src/test/java/com/bloomingbread/blockchain/crypto/keygenerator/MessageDigestWrapperTest.java
+++ src/test/java/com/bloomingbread/blockchain/crypto/keygenerator/MessageDigestWrapperTest.java
@@ -3,21 +3,20 @@ package com.bloomingbread.blockchain.crypto.keygenerator;
 import com.bloomingbread.blockchain.crypto.CryptoByteUtils;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
 
 public class MessageDigesWrapperTest {
     @Test
     public void digestWithDefalut() throws Exception {
         System.out.println(MessageDigesWrapper.DEFAULT_ALGORITHM);
         MessageDigesWrapper digestUtils = new MessageDigesWrapper();
-        String message1 = CryptoByteUtils.randomString(56);
-        byte[] digest1 = digestUtils.digest(message1.getBytes("UTF-8"));
+        String message = CryptoByteUtils.randomString(56);
+        byte[] digest1 = digestUtils.digest(message.getBytes("UTF-8"));
+        byte[] digest2 = digestUtils.digest(message.getBytes("UTF-8"));
 
-        String message2 = CryptoByteUtils.randomString(1) + message1.substring(1) ;
-        byte[] digest2 = digestUtils.digest(message2.getBytes("UTF-8"));
-
-        assertEquals(digest1, digest2);
+        assertArrayEquals(digest1, digest2);
     }
 
     @Test
@@ -25,13 +24,11 @@ public class MessageDigesWrapperTest {
         String digestAlgorithm = "MD5";
         System.out.println(digestAlgorithm);
         MessageDigesWrapper digestUtils = new MessageDigesWrapper();
-        String message1 = CryptoByteUtils.randomString(56);
-        byte[] digest1 = digestUtils.digest(message1.getBytes("UTF-8"), digestAlgorithm);
+        String message = CryptoByteUtils.randomString(56);
+        byte[] digest1 = digestUtils.digest(message.getBytes("UTF-8"), digestAlgorithm);
+        byte[] digest2 = digestUtils.digest(message.getBytes("UTF-8"), digestAlgorithm);
 
-        String message2 = CryptoByteUtils.randomString(1) + message1.substring(1) ;
-        byte[] digest2 = digestUtils.digest(message2.getBytes("UTF-8"), digestAlgorithm);
-
-        assertEquals(digest1, digest2);
+        assertArrayEquals(digest1, digest2);
     }
 
     @Test
@@ -45,8 +42,9 @@ public class MessageDigesWrapperTest {
         String message2 = CryptoByteUtils.randomString(1) + message1.substring(1) ;
         byte[] digest2 = digestUtils.digest(message2.getBytes("UTF-8"), digestAlgorithm2);
 
-        assertNotEquals(message1, message2);
+        assertFalse(Arrays.equals(digest1, digest2));
     }
+
     @Test
     public void findMessageDigestAlgorithm() throws Exception {
         String digestAlgorithm = "SHA-256";
